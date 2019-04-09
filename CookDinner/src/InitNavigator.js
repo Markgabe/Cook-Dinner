6d81e656e08306804fd9694617b3020fc413d83a
Marcos Gabriel <39177873+Markgabe@users.noreply.github.com>
2019-04-09 11:01:20 -0300

    Drawing the screens

--- CookDinner/src/InitNavigator.js
+++ CookDinner/src/InitNavigator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, View, Text } from 'react-native';
 import Login from './LoginScreen';
 import Register from './RegisterScreen';
-import Home from './HomeScreen';
+import Home from './StartScreen';
 import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
 
 
--- CookDinner/src/RegisterScreen.js
+++ CookDinner/src/RegisterScreen.js
@@ -1,18 +1,58 @@
 import React from 'react';
-import { Text, View, Button, TextInput } from 'react-native';
+import { Text, View, TouchableHighlight, TextInput, Image, StyleSheet } from 'react-native';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import styles from './styles';
 
 export default class Register extends React.Component {
+
+  static navigationOptions = { header: null };
+
+  constructor(props) {
+      super(props);
+      this.state = { username: '', password: '' };
+    }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text
-        style={{
-          fontSize: 30,
-        }}
-        >Cadastre-se</Text>
-      </View>
+      <View style={styles.mainView}>
 
+        <View style={styles.topView}>
+          <Image source={require('./img/comida.png')} style={styles.image}/>
+          <Text style={styles.titleText}>Cook Dinner</Text>
+        </View>
+
+        <View style={{flex: 1, paddingTop: 60, alignItems: 'center'}}>
+
+          <View style={styles.textInputView}>
+
+            <TextInput style={styles.textInput}
+            placeholder="Username"
+            onChangeText={(username) => this.setState({username})}
+            value={this.state.username}
+            />
+
+            <TextInput style={styles.textInput}
+            placeholder="Password"
+            onChangeText={(password) => this.setState({password})}
+            value={this.state.password}
+            />
+
+          </View>
+
+          <View style= {styles.buttonView}>
+
+            <TouchableHighlight
+              style={styles.button}
+              onPress={() => alert()}
+              underlayColor='#fff'>
+              <Text style={styles.buttonText}>Log in</Text>
+            </TouchableHighlight>
+
+          </View>
+
+        </View>
+
+      </View>
     );
   }
 }
