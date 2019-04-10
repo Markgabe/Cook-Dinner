6c64cac760661ece2e319ee6a405e224e051a067
Gurja69 <leonardogmf@gmail.com>
2019-04-10 19:55:01 -0300

    top bar sem icone

--- CookDinner/src/Home.js
+++ CookDinner/src/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import styles from './styles';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const newRecipe = () => {
-
-};
+const icon = <FontAwesome5 name={'comments'} solid/>
 
 export default class Register extends React.Component {
 
@@ -18,22 +17,26 @@ export default class Register extends React.Component {
       this.state = { search: '' };
     }
 
+    newRecipe() {
+
+    };
+
     render() {
         return (
-        //Screen
+
         <View style={{fontSize: 30, fontWeight: 'bold', flex: 1}}>
-            //Top Bar
-            <View style={{alignItems: 'center', backgroundColor: 'gray'}}>
-                //search bar
+
+            <View style={{backgroundColor: 'gray', flexDirection: 'row', alignItems: 'center'}}>
+
                 <TextInput style={ styles.searchInput }
                     placeholder="Pesquisar"
                     onChangeText={(search) => this.setState({search})}
                     value={this.state.search}
                 />
-                //New Recipe Button
-                <TouckableOpacity style={{alignSelf: 'self-end'}}, onPress={newRecipe}>
-                    <Icon name='fas fa-plus' size={30} color='#FFF' />
-                </TouckableOpacity>
+
+                <TouchableOpacity onPress={() => this.newRecipe()} style={{alignSelf:'center', marginLeft: 'auto', marginRight: 15 }}>
+                    <Icon name='comments' size={36} color='#FFF' />
+                </TouchableOpacity>
             </View>
 
             <View style={{fontSize: 30, fontWeight: 'bold', backgroundColor: 'Gray'}}>
